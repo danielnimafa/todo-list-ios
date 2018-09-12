de56d56a8c4d6a59021ff92c05e4a773dd6d5dfa
Daniel <danielnimafa@gmail.com>
2018-09-12 14:37:52 +0700

    create model data

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Daniel Nimafa on 12/09/18.
+//  Copyright © 2018 Kipacraft. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
--- Todoey/TodoListVC.swift
+++ Todoey/TodoListVC.swift
@@ -9,42 +9,73 @@
 import UIKit
 
 class TodoListVC: UITableViewController {
-
-    var itemArray = ["Find Mike", "Mangan", "Sego"]
+    
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
+    
+    let ARRAY_TAG = "TodoListData"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+        // populateData()
+        
+        let item1 = Item()
+        item1.title = "Bejo"
+        itemArray.append(item1)
+        
+        let item2 = Item()
+        item2.title = "Sugeng"
+        itemArray.append(item2)
+        
+        let item3 = Item()
+        item3.title = "Domblang"
+        itemArray.append(item3)
+        
+//        if let items = defaults.array(forKey: ARRAY_TAG) as? [Item] {
+//            itemArray = items
+//            print("loading data from user defaults")
+//        }
         
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        print("numberOfRowsInSection: \(item.done)")
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("selected \(itemArray[indexPath.row ])")
-                
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-           tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        
+        let item = itemArray[indexPath.row]
+        item.done = !item.done
+        
+        tableView.cellForRow(at: indexPath)?.accessoryType = item.done ? .checkmark : .none
+        
+        print("Select Action: \(item.done)")
+        
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        } else {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
+        
+//        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
+        
     }
     
     @IBAction func addBtnPressed(_ sender: UIBarButtonItem) {
@@ -56,9 +87,12 @@ class TodoListVC: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             print("New Item : \(textField.text!)")
             
-            self.itemArray.append(textField.text ?? "new item")
+            let newItem = Item()
+            newItem.title = textField.text!
             
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            self.itemArray.append(newItem)
+            
+            self.defaults.set(self.itemArray, forKey: self.ARRAY_TAG)
             
             self.tableView.reloadData()
         }
@@ -74,5 +108,22 @@ class TodoListVC: UITableViewController {
         
     }
     
+    private func populateData() {
+        
+//        let item1 = Item("Find my mac", false)
+//        let item2 = Item("Build something i care about", false)
+//        let item3 = Item("Mastering iOS Development", false)
+//
+//        itemArray.append(item1)
+//        itemArray.append(item2)
+//        itemArray.append(item3)
+//
+//        for number in 0...20 {
+//            itemArray.append(Item("\(number)", false))
+//        }
+        
+    }
+    
 }
+
 
